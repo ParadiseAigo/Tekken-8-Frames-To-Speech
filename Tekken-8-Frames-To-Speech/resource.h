35d0232ff0483bb70878891f943673036cc59be7
Aigo Paradise <ParadiseAigo@hotmail.com>
2024-02-22 14:03:54 +0100

    added exe icon

--- Tekken-8-Frames-To-Speech/resource.h
+++ Tekken-8-Frames-To-Speech/resource.h
@@ -22,12 +22,13 @@
 #define IDR_WAVE18                      118
 #define IDR_WAVE19                      119
 #define IDR_WAVE20                      120
+#define IDI_ICON1                       121
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        121
+#define _APS_NEXT_RESOURCE_VALUE        122
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
